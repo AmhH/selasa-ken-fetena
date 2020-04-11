cc8709a9fc7b0fdb15d843f266e216d57dcb897b
Amh <abaams78@gmail.com>
2020-04-10 21:21:02 -0500

    aseregna ken new alu

---
+++ src/main/java/com/example/leet/week2/Day10.java
@@ -0,0 +1,69 @@
+package com.example.leet.week2;
+
+import java.util.Stack;
+
+/**
+ * Min Stack
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ *
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ *
+ * Example:
+ *
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ *
+ *    Show Hint #1
+ *    Consider each node in the stack having a minimum value. (Credits to @aakarshmadhavan)
+ */
+public class Day10 {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());   // Returns -3.
+        minStack.pop();
+        System.out.println(minStack.top());      // Returns 0.
+        System.out.println(minStack.getMin());   // Returns -2.
+    }
+}
+
+class MinStack {
+
+    Stack<int[]> stack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        if(stack.isEmpty()){
+            stack.push(new int[] {x, x});
+        }else{
+            stack.push(new int[] {x, Math.min(stack.peek()[1], x)});
+        }
+
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
