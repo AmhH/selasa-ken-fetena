0dc52ec2820deee409afe89bd8d8af28f8895e38
Amh <abaams78@gmail.com>
2020-04-18 22:51:53 -0500

    17 ken soln

--- src/main/java/com/example/leet/week3/Day17.java
+++ src/main/java/com/example/leet/week3/Day17.java
@@ -33,5 +33,37 @@ public class Day17 {
      * @param grid zd array
      * @return number of islands
      */
+    public static int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+        else{
+            int R = grid.length;
+            int C = grid[0].length;
+            int i, j, count = 0;
+
+            for(i = 0; i < R; i++){
+                for(j = 0; j < C; j++){
+                    if(grid[i][j] == '1'){
+                        dfs(grid, i, j, R, C);
+                        ++count;
+                    }
+
+                }
+            }
+
+            return count;
+        }
+    }
+
+    private static void dfs(char[][] grid, int x, int y, int R, int C){
+        if(x >= 0 && x < R && y >= 0 && y < C && grid[x][y] == '1'){
+            grid[x][y] = '2';
+            dfs(grid, x + 1, y, R, C);
+            dfs(grid, x - 1, y, R, C);
+            dfs(grid, x, y + 1, R, C);
+            dfs(grid, x, y - 1, R, C);
+        }
+    }
+
 
 }
