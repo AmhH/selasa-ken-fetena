a4a60959fda6d373632484dc852fb0a0025cdcb3
Amh <abaams78@gmail.com>
2020-04-18 22:51:22 -0500

    17 ken

---
+++ src/main/java/com/example/leet/week3/Day17.java
@@ -0,0 +1,37 @@
+package com.example.leet.week3;
+
+import java.util.Arrays;
+
+/**
+ * Number of Islands
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water
+ * and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are
+ * all surrounded by water.
+ *
+ * Example 1:
+ *
+ * Input:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ *
+ * Output: 1
+ * Example 2:
+ *
+ * Input:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ *
+ * Output: 3
+ */
+public class Day17 {
+    /**
+     * https://www.geeksforgeeks.org/find-number-of-islands/
+     * @param grid zd array
+     * @return number of islands
+     */
+
+}
