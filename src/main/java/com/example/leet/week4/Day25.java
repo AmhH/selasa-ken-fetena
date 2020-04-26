45b218d8536bb8222c121ba273604406322e9e61
Amh <abaams78@gmail.com>
2020-04-26 18:03:59 -0500

    25 ken soln2

--- src/main/java/com/example/leet/week4/Day25.java
+++ src/main/java/com/example/leet/week4/Day25.java
@@ -45,4 +45,14 @@ public class Day25 {
 
         return false;
     }
+
+    public boolean canJump2(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
 }
