685667322d901b19c499c8086ae45700f48302ba
Amh <abaams78@gmail.com>
2020-05-02 22:35:19 -0500

    2 tejemere aleke

--- src/main/java/com/example/leet/may/week1/Day2.java
+++ src/main/java/com/example/leet/may/week1/Day2.java
@@ -1,13 +1,45 @@
 package com.example.leet.may.week1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ *Jewels and Stones
+ * You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
+ * Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
  *
+ * The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive,
+ * so "a" is considered a different type of stone from "A".
+ *
+ * Example 1:
+ *
+ * Input: J = "aA", S = "aAAbbbb"
+ * Output: 3
+ * Example 2:
+ *
+ * Input: J = "z", S = "ZZ"
+ * Output: 0
+ * Note:
+ *
+ * S and J will consist of letters and have length at most 50.
+ * The characters in J are distinct.
  */
 public class Day2 {
 
     public int numJewelsInStones(String J, String S) {
-        Map<String, String> map = new HashMap<>();
-        for(char c)
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < J.length(); i++) {
+            map.put(J.charAt(i), J.charAt(i));
+        }
+
+        int count = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if(map.get(S.charAt(i)) != null){
+                count++;
+            }
+        }
+
+        return count;
 
     }
 }
