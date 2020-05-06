b11d9a5100025e3255ab5d4a38c1b6230865d5bc
Amh <abaams78@gmail.com>
2020-05-05 22:14:58 -0500

    5 ken soln

--- src/main/java/com/example/leet/may/week1/Day5.java
+++ src/main/java/com/example/leet/may/week1/Day5.java
@@ -1,5 +1,8 @@
 package com.example.leet.may.week1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * First Unique Character in a String
  * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
@@ -14,4 +17,22 @@ package com.example.leet.may.week1;
  * Note: You may assume the string contain only lowercase letters.
  */
 public class Day5 {
+    public static int firstUniqChar(String s) {
+        int[] arr = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            arr[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(arr[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("leetcode"));
+        System.out.println(firstUniqChar("loveleetcode"));
+        System.out.println(firstUniqChar("cc"));
+    }
 }
