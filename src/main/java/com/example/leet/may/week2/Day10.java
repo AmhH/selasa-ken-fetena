009376b8a1e879c2169ad862f6fb92f78537b854
Amh <abaams78@gmail.com>
2020-05-11 18:58:06 -0500

    ken 10 soln

--- src/main/java/com/example/leet/may/week2/Day10.java
+++ src/main/java/com/example/leet/may/week2/Day10.java
@@ -2,40 +2,38 @@ package com.example.leet.may.week2;
 
 /**
  *  Find the Town Judge
- * In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+ * In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town
+ * judge.
  *
  * If the town judge exists, then:
  *
  * The town judge trusts nobody.
  * Everybody (except for the town judge) trusts the town judge.
  * There is exactly one person that satisfies properties 1 and 2.
- * You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+ * You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person
+ * labelled b.
  *
  * If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
  *
- *
- *
  * Example 1:
- *
  * Input: N = 2, trust = [[1,2]]
  * Output: 2
- * Example 2:
  *
+ * Example 2:
  * Input: N = 3, trust = [[1,3],[2,3]]
  * Output: 3
- * Example 3:
  *
+ * Example 3:
  * Input: N = 3, trust = [[1,3],[2,3],[3,1]]
  * Output: -1
- * Example 4:
  *
+ * Example 4:
  * Input: N = 3, trust = [[1,2],[2,3]]
  * Output: -1
- * Example 5:
  *
+ * Example 5:
  * Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
  * Output: 3
- *
  *
  * Note:
  *
@@ -47,7 +45,7 @@ package com.example.leet.may.week2;
  */
 public class Day10 {
 
-    public int findJudge(int N, int[][] trust) {
+    public static int findJudge(int N, int[][] trust) {
         int[] in = new int[N + 1];
         int[] out = new int[N + 1];
 
@@ -63,5 +61,13 @@ public class Day10 {
         }
 
         return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findJudge(2, new int[][]{{1,2}})); //2
+        System.out.println(findJudge(3, new int[][]{{1,3},{2,3}})); //3
+        System.out.println(findJudge(3, new int[][]{{1,3},{2,3},{3,1}})); //-1
+        System.out.println(findJudge(3, new int[][]{{1,2},{2,3}})); //-1
+        System.out.println(findJudge(4, new int[][]{{1,3},{1,4},{2,3},{2,4},{4,3}})); //3
     }
 }
