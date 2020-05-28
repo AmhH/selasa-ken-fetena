6e03fc9ecab141796643c426196e601caf8d4ea4
Amh <abaams78@gmail.com>
2020-05-27 23:15:14 -0500

    27 soln1

--- src/main/java/com/example/leet/may/week4/Day27.java
+++ src/main/java/com/example/leet/may/week4/Day27.java
@@ -71,5 +71,31 @@ public class Day27 {
         System.out.println(possibleBipartition(5, new int[][]{{1,2},{2,3},{3,4},{4,5},{1,5}}));
     }
 
+    public boolean possibleBipartition1(int N, int[][] dislikes) {
+        int[] ids = new int[N+1];
+        for(int i=0; i < ids.length; i++) {
+            ids[i] = i;
+        }
+
+        for(int[] x: dislikes) {
+            int one = x[0];
+            int two = x[1];
+
+            if (ids[one] == one && ids[two] == two) {
+                ids[one] = two;
+                ids[two] = one;
+            }
+            else if (ids[one] != one && ids[two] == two) {
+                ids[two] = ids[ids[one]];
+            }
+            else if (ids[one] == one && ids[two] != two) {
+                ids[one] = ids[ids[two]];
+            }
+            else if (ids[one] != one && ids[two] != two && ids[one] == ids[two]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
