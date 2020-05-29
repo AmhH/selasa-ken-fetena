72e9d86ec5f8acc7c0a41815ae1543a22e7600c1
Amh <abaams78@gmail.com>
2020-05-28 23:19:51 -0500

    ken 28 soln

--- src/main/java/com/example/leet/may/week4/Day28.java
+++ src/main/java/com/example/leet/may/week4/Day28.java
@@ -1,5 +1,7 @@
 package com.example.leet.may.week4;
 
+import java.util.Arrays;
+
 /**
  * Counting Bits
  * Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's
@@ -29,4 +31,15 @@ package com.example.leet.may.week4;
  * Or does the odd/even status of the number help you in calculating the number of 1s?
  */
 public class Day28 {
+    public static int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for(int i = 1; i < num + 1; i++)
+            ans[i] = ans[i & (i - 1)] + 1;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(countBits(2)));
+        System.out.println(Arrays.toString(countBits(5)));
+    }
 }
