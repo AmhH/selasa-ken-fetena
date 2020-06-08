fd70d4b7931aab05f4a498c9b3c6f31832a4efa7
Amh <abaams78@gmail.com>
2020-06-07 22:30:32 -0500

    Coin Change 2 3

--- src/main/java/com/example/leet/june/week1/Day7.java
+++ src/main/java/com/example/leet/june/week1/Day7.java
@@ -36,5 +36,18 @@ package com.example.leet.june.week1;
  * the answer is guaranteed to fit into signed 32-bit integer
  */
 public class Day7 {
+    public static int change(int amount, int[] coins) {
+        int arr[] = new int[amount+1];
+        arr[0] = 1;
+        for(int c : coins)
+            for(int i = c; i <= amount; i++)
+                arr[i] += arr[i-c];
+        return arr[amount];
+    }
 
+    public static void main(String[] args) {
+        System.out.println(change( 5, new int[] {1, 2, 5}));
+        System.out.println(change( 3, new int[] {2}));
+        System.out.println(change( 10, new int[] {10}));
+    }
 }
