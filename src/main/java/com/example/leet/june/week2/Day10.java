c87714e68263a69a7402ee1738939139b13cc33e
Amh <abaams78@gmail.com>
2020-06-10 22:00:33 -0500

    search insert position soln

--- src/main/java/com/example/leet/june/week2/Day10.java
+++ src/main/java/com/example/leet/june/week2/Day10.java
@@ -8,25 +8,45 @@ package com.example.leet.june.week2;
  * You may assume no duplicates in the array.
  *
  * Example 1:
- *
  * Input: [1,3,5,6], 5
  * Output: 2
- * Example 2:
  *
+ * Example 2:
  * Input: [1,3,5,6], 2
  * Output: 1
- * Example 3:
  *
+ * Example 3:
  * Input: [1,3,5,6], 7
  * Output: 4
- * Example 4:
  *
+ * Example 4:
  * Input: [1,3,5,6], 0
  * Output: 0
  */
 public class Day10 {
 
-    public int searchInsert(int[] nums, int target) {
+    public static int searchInsert(int[] nums, int target) {
 
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 5));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 2));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 7));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 0));
     }
 }
