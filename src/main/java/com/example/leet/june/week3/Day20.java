123cad0cd57b036837acae54cd66b995f5df6fde
Amh <abaams78@gmail.com>
2020-06-20 23:09:08 -0500

    Permutation Sequence soln

--- src/main/java/com/example/leet/june/week3/Day20.java
+++ src/main/java/com/example/leet/june/week3/Day20.java
@@ -1,5 +1,8 @@
 package com.example.leet.june.week3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Permutation Sequence
  * The set [1,2,3,...,n] contains a total of n! unique permutations.
@@ -29,7 +32,56 @@ package com.example.leet.june.week3;
  */
 public class Day20 {
 
-    public String getPermutation(int n, int k) {
+    public static String getPermutation (int n, int k) {
+
+        int[] factorial = new int[n];
+        List<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            nums.add (i + 1);
+            factorial[i] = i == 0 ? 1 : i * factorial[i - 1];
+        }
+
+        StringBuilder ans = new StringBuilder ();
+        while (n-- != 0) {
+            ans.append (nums.remove ((k - 1) / factorial[n]));
+            k = (k - 1) % factorial[n] + 1;
+        }
+
+        return ans.toString ();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getPermutation(3, 3));
+        System.out.println(getPermutation(4, 9));
+    }
+
+    class Solution {
+        // 18 mins. 2020/06/19
+        public String getPermutation(int n, int k) {
+            int parts[] = new int[n];
+            k--;
+            for (int i=n-1; i>=1; i--) {
+                int f = fact(i);
+                parts[n-1-i] = k / f;
+                k = k % f;
+            }
+            List<Integer> nums = new ArrayList<>();
+            for (int i=1; i<=n; i++)
+                nums.add(i);
+            String res = "";
+            for (int i=0; i<n; i++) {
+                res += nums.get(parts[i]) + "";
+                nums.remove(parts[i]);
+            }
+            return res;
+        }
+        private int fact(int n){
+            int res = 1;
+            for (int i=1; i<=n; i++)
+                res *= i;
+            return res;
+        }
 
     }
 }
