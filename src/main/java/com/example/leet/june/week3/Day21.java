6cab826e9d3a0428939847e28030b2b3201d3654
Amh <abaams78@gmail.com>
2020-06-21 22:59:48 -0500

    Dungeon Game soln

--- src/main/java/com/example/leet/june/week3/Day21.java
+++ src/main/java/com/example/leet/june/week3/Day21.java
@@ -1,5 +1,7 @@
 package com.example.leet.june.week3;
 
+import java.util.Arrays;
+
 /**
  * Dungeon Game
  * The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon. The dungeon
@@ -35,7 +37,92 @@ package com.example.leet.june.week3;
  */
 public class Day21 {
 
-    public int calculateMinimumHP(int[][] dungeon) {
+    public static int calculateMinimumHP(int[][] dungeon) {
+        int[][] dp = new int[dungeon.length + 1][dungeon[0].length + 1];
+        for(int i = 0; i < dungeon.length + 1; i++) Arrays.fill(dp[i], -1);
 
+        return mhr(dungeon, 0, 0, dp);
+    }
+
+    private static int mhr(int[][] arr, int i, int j, int[][] dp){
+        if(dp[i][j] != -1) return dp[i][j];
+        if(i == arr.length - 1 && j == arr[0].length - 1) return arr[i][j] > 0 ? 1 : Math.abs(arr[i][j]) + 1;
+        if(i == arr.length || j == arr[0].length) return Integer.MAX_VALUE;
+
+        return dp[i][j] = Math.max(1, Math.min(mhr(arr, i + 1, j, dp), mhr(arr, i, j + 1, dp)) - arr[i][j]);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateMinimumHP(new int[][]{{-2, -3, 3}, {-5, -10, 1}, {10, 30, -5}}));
+    }
+
+    class Solution {
+        public int calculateMinimumHP(int[][] dungeon) {
+            return mhr(dungeon, 0, 0);
+        }
+
+        int mhr(int[][] arr, int i, int j){
+            if(i == arr.length - 1 && j == arr[0].length - 1) return arr[i][j] > 0 ? 1 : Math.abs(arr[i][j]) + 1;
+            if(i == arr.length || j == arr[0].length) return Integer.MAX_VALUE;
+
+            return Math.max(1, Math.min(mhr(arr, i + 1, j), mhr(arr, i, j + 1)) - arr[i][j]);
+        }
+    }
+
+    public int calculateMinimumHP2(int[][] dungeon) {
+        int n = dungeon.length;
+        int m = dungeon[0].length;
+        int[] dp = new int[m];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (i + 1 == n && j + 1 == m) {
+                    dp[j] = Math.max(1 - dungeon[i][j], 1);
+                    continue;
+                }
+
+                if (i + 1 == n) {
+                    dp[j] = Math.max(dp[j + 1] - dungeon[i][j], 1);
+                } else if (j + 1 == m) {
+                    dp[j] = Math.max(dp[j] - dungeon[i][j], 1);
+                } else {
+                    int right = Math.max(dp[j + 1] - dungeon[i][j], 1);
+                    int down = Math.max(dp[j] - dungeon[i][j], 1);
+                    dp[j] = Math.min(right, down);
+                }
+            }
+        }
+
+        return dp[0];
+    }
+
+    class Solution2 {
+        public int calculateMinimumHP(int[][] dungeon) {
+            if(dungeon == null || dungeon.length==0 || dungeon[0]==null || dungeon[0].length==0) {
+                return 0;
+            }
+            int m = dungeon.length, n = dungeon[0].length;
+
+            int[][] dp = new int[m][n];
+            dp[m-1][n-1] = Math.max(1-dungeon[m-1][n-1], 1);
+
+            return dfs(dungeon, dp, 0, 0);
+        }
+
+        private int dfs(int[][] dungeon, int[][] dp, int x, int y) {
+            int m = dungeon.length;
+            int n = dungeon[0].length;
+
+            if(x == m || y == n) return Integer.MAX_VALUE/2;
+            if(dp[x][y] > 0) return dp[x][y];
+
+            int right = Math.max(dfs(dungeon, dp, x, y+1) - dungeon[x][y], 1);
+            int down = Math.max(dfs(dungeon, dp, x+1, y) - dungeon[x][y], 1);
+
+            dp[x][y] = Math.min(right, down);
+
+            return dp[x][y];
+
+        }
     }
 }
