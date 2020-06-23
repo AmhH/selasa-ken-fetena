82c54ba0699f4f605220cdefb41aeb65f9957191
Amh <abaams78@gmail.com>
2020-06-23 16:25:06 -0500

    Count Complete Tree Nodes soln

--- src/main/java/com/example/leet/june/week4/Day23.java
+++ src/main/java/com/example/leet/june/week4/Day23.java
@@ -23,5 +23,55 @@ package com.example.leet.june.week4;
  */
 public class Day23 {
 
+    public static int countNodes(TreeNode root) {
+        if(root == null)
+            return 0;
+        int l = countNodes(root.left);
+        int r = countNodes(root.right);
+        return l + r + 1;
+    }
+
+    public static int countNodes2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        } else if (root.left == null && root.right != null) {
+            return 1 + countNodes(root.right);
+        } else if (root.left != null && root.right == null) {
+            return 1 + countNodes(root.left);
+        } else {
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node2 = new TreeNode(2, new TreeNode(4), new TreeNode(5));
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(6);
+        node3.left = node6;
+        TreeNode node1 = new TreeNode(2, node2, node3);
+
+        System.out.println(countNodes(node1));
+        System.out.println(countNodes2(node1));
+    }
+
+    /**
+     * Definition for a binary tree node.
+     */
+      public static class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
 
 }
