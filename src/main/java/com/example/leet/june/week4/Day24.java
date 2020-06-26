021ad96cb6e48aeff0f9e9e5e4888d596a5072e9
Amh <abaams78@gmail.com>
2020-06-25 20:26:52 -0500

    unique binary search trees soln

--- src/main/java/com/example/leet/june/week4/Day24.java
+++ src/main/java/com/example/leet/june/week4/Day24.java
@@ -1,5 +1,8 @@
 package com.example.leet.june.week4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *  Unique Binary Search Trees
  * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
@@ -18,12 +21,24 @@ package com.example.leet.june.week4;
  *    2     1         2                 3
  */
 public class Day24 {
-
-    public static int numTrees(int n) {
-
+    Map<Integer, Integer> map = new HashMap();
+    public int numTrees(int n) {
+        if(n == 0 || n == 1)
+            return 1;
+        if(map.containsKey(n))
+            return map.get(n);
+        int count = 0;
+        for(int i = 1; i <= n; i++)
+            count += numTrees(i - 1) * numTrees(n - i);
+        map.put(n, count);
+        return map.get(n);
     }
 
     public static void main(String[] args) {
-        System.out.println(numTrees(3));
+        System.out.println(new Day24().numTrees(3));
+        System.out.println(new Day24().numTrees(4));
+        long start = System.currentTimeMillis();
+        System.out.println(new Day24().numTrees(19));
+        System.out.println((System.currentTimeMillis()-start)/1000);
     }
 }
