ea3aa2bed09a68bcf6bb0ef5e28ca16c1fdec094
Amh <abaams78@gmail.com>
2020-06-25 20:52:55 -0500

    Find the Duplicate Number soln2

--- src/main/java/com/example/leet/june/week4/Day25.java
+++ src/main/java/com/example/leet/june/week4/Day25.java
@@ -47,4 +47,24 @@ public class Day25 {
         System.out.println(findDuplicate(new int[]{1,3,4,2,2}));
         System.out.println(findDuplicate(new int[]{3,1,3,4,2}));
     }
+    class Solution {
+        public int findDuplicate(int[] nums) {
+            // Find the intersection point of the two runners.
+            int tortoise = nums[0];
+            int hare = nums[0];
+            do {
+                tortoise = nums[tortoise];
+                hare = nums[nums[hare]];
+            } while (tortoise != hare);
+
+            // Find the "entrance" to the cycle.
+            tortoise = nums[0];
+            while (tortoise != hare) {
+                tortoise = nums[tortoise];
+                hare = nums[hare];
+            }
+
+            return hare;
+        }
+    }
 }
