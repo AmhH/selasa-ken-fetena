93ca5f8210b255da05076d6c645d4ca50e03d5c2
Amh <abaams78@gmail.com>
2020-06-27 14:09:30 -0500

    Sum root to leaf Numbers son

--- src/main/java/com/example/leet/june/week4/Day26.java
+++ src/main/java/com/example/leet/june/week4/Day26.java
@@ -38,5 +38,40 @@ package com.example.leet.june.week4;
  */
 public class Day26 {
 
+    public int sumNumbers(TreeNode root) {
+        return sumNumbers(root, 0);
+    }
 
+    private int sumNumbers(TreeNode root, int sum) {
+        if(root == null)
+            return 0;
+        int current = sum * 10 + root.val;
+        if(root.left == null && root.right == null)
+            return current;
+
+        return sumNumbers(root.left, current) + sumNumbers(root.right, current);
+    }
+
+    /**
+     * Definition for a binary tree node.
+     */
+      public static class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(9, new TreeNode(5), new TreeNode(1));
+        TreeNode root = new TreeNode(4, node, new TreeNode(0));
+
+        System.out.println(new Day26().sumNumbers(root));
+    }
 }
