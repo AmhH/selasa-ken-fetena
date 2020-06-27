937cc49cd27110a7c458842e35a58cbd8e791334
Amh <abaams78@gmail.com>
2020-06-27 14:11:11 -0500

    Perfect Squares

---
+++ src/main/java/com/example/leet/june/week4/Day27.java
@@ -0,0 +1,29 @@
+package com.example.leet.june.week4;
+
+/**
+ * Perfect Squares
+ * Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which
+ * sum to n.
+ *
+ * Example 1:
+ *
+ * Input: n = 12
+ * Output: 3
+ * Explanation: 12 = 4 + 4 + 4.
+ * Example 2:
+ *
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ */
+public class Day27 {
+
+    public static int numSquares(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numSquares(12));
+        System.out.println(numSquares(13));
+    }
+}
