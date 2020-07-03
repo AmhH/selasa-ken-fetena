16f6e7f0067f496064f1ed3420f605ab441c7e33
Amh <abaams78@gmail.com>
2020-07-02 22:05:25 -0500

    Binary Tree Level Order Traversal soln

--- src/main/java/com/example/leet/july/week1/Day2.java
+++ src/main/java/com/example/leet/july/week1/Day2.java
@@ -1,5 +1,8 @@
 package com.example.leet.july.week1;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *  Binary Tree Level Order Traversal II
  * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right,
@@ -20,7 +23,42 @@ package com.example.leet.july.week1;
  * ]
  */
 public class Day2 {
-    public static void main(String[] args) {
 
+    public List<List<Integer>> levelOrderBottom (TreeNode root) {
+
+        List<List<Integer>> ans = new LinkedList<>();
+        helper (root, 0, ans);
+        return ans;
     }
+
+    private void helper (TreeNode root, int depth, List<List<Integer>> ans) {
+
+        if (root == null) {
+            return;
+        }
+        else if (depth == ans.size ()) {
+            ans.add (0, new LinkedList<> ());
+        }
+        ans.get (ans.size () - depth - 1).add (root.val);
+        helper (root.left, depth + 1, ans);
+        helper (root.right, depth + 1, ans);
+    }
+
+
+    /**
+     * Definition for a binary tree node.
+     */
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
 }
