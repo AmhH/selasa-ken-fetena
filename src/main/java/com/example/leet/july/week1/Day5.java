0fcf4d1c52ad9493db48a73950412657af9c203c
Amh <abaams78@gmail.com>
2020-07-05 17:27:38 -0500

    Hamming Distance soln

--- src/main/java/com/example/leet/july/week1/Day5.java
+++ src/main/java/com/example/leet/july/week1/Day5.java
@@ -24,4 +24,18 @@ package com.example.leet.july.week1;
  */
 public class Day5 {
 
+    public static int hammingDistance(int x, int y) {
+        int result = 0;
+        while(x > 0 || y > 0){
+            result += x%2 ^ y%2;
+            x = x >> 1;
+            y = y >> 1;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hammingDistance(1, 4));
+    }
 }
