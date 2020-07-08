9161ec54a53a7a9e568f13ec1c9994053bb0155e
Amh <abaams78@gmail.com>
2020-07-07 22:02:06 -0500

    Island perimeter soln

--- src/main/java/com/example/leet/july/week1/Day7.java
+++ src/main/java/com/example/leet/july/week1/Day7.java
@@ -26,4 +26,57 @@ package com.example.leet.july.week1;
  * Explanation: The perimeter is the 16 yellow stripes in the image below:
  */
 public class Day7 {
+
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        if(grid == null || grid.length == 0)
+            return perimeter;
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column < grid[0].length; column++) {
+                if(grid[row][column] == 1){
+                    perimeter += 4;
+                    if(row > 0) perimeter -= grid[row - 1][column];
+                    if (row < grid.length - 1) perimeter -= grid[row + 1][column];
+                    if(column > 0) perimeter -= grid[row][column - 1];
+                    if (column < grid[0].length - 1) perimeter -= grid[row][column + 1];
+                }
+            }
+        }
+
+        return perimeter;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Day7().islandPerimeter(new int[][]{
+                    {0,1,0,0},
+                    {1,1,1,0},
+                    {0,1,0,0},
+                    {1,1,0,0}}));
+    }
+
+    class Solution {
+        public int islandPerimeter(int[][] grid) {
+            int rows = grid.length;
+            int cols = grid[0].length;
+
+            int result = 0;
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < cols; c++) {
+                    if (grid[r][c] == 1) {
+                        result += 4;
+
+                        if (r > 0 && grid[r-1][c] == 1) {
+                            result -= 2;
+                        }
+
+                        if (c > 0 && grid[r][c-1] == 1) {
+                            result -= 2;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
 }
