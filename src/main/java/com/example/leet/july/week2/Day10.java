2945f4de52fdd812fe7829cb076c52b792ea8d2f
Amh <abaams78@gmail.com>
2020-07-10 22:10:08 -0500

    Flatten a multilevel Doubly linked list solns

--- src/main/java/com/example/leet/july/week2/Day10.java
+++ src/main/java/com/example/leet/july/week2/Day10.java
@@ -75,6 +75,71 @@ import java.util.Stack;
  */
 public class Day10 {
 
+    public Node flatten(Node head) {
+        if(head == null || (head.next == null && head.child == null))
+            return head;
+        Stack<Node> stack = new Stack<>();
+        Node temp = head;
+        while (temp != null || !stack.isEmpty()){
+            if(temp.child != null){
+                if(temp.next != null)
+                    stack.push(temp.next);
+                temp.next = temp.child;
+                temp.child.prev = temp;
+                temp.child = null;
+            }else if(temp.next == null && !stack.isEmpty()){
+                temp.next = stack.pop();
+                temp.next.prev = temp;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        /**
+         * [1,2,3,4,5,6,null]
+         * [7,8,9,10,null]
+         * [11,12,null]
+         */
+        Node l1 = createNode(new int[]{1,2,3,4,5,6});
+        Node l2 = createNode(new int[]{7,8,9,10});
+        Node l3 = createNode(new int[]{11,12});
+        attachNode(l2, l3, 1);
+        attachNode(l1, l2, 2);
+        System.out.println("Hello");
+        Node flatten = new Day10().flatten(l1);
+        System.out.println("Done");
+
+        while (flatten != null){
+            System.out.print(flatten.val + " ");
+            if(flatten.child != null)
+                System.err.println("WRONG");
+            flatten = flatten.next;
+        }
+
+    }
+
+    private static void attachNode(Node parent, Node child, int index) {
+        while (--index >= 0){
+            parent = parent.next;
+        }
+
+        parent.child = child;
+    }
+
+    public static Node createNode(int[] array){
+        Node head = new Node(array[0]);
+        Node tail = head;
+        for (int i = 1; i < array.length; i++) {
+            Node node = new Node(array[i]);
+            tail.next = node;
+            node.prev = tail;
+            tail = tail.next;
+        }
+
+        return head;
+    }
     /*
 // Definition for a Node.
 */
@@ -87,5 +152,52 @@ static class Node {
         this.val = val;
     }
 }
+
+    class Solution {
+        public Node flatten(Node head) {
+            if (head == null) return head;
+            Node current = head;
+            while (current != null) {
+                if (current.child == null) {
+                    current = current.next;
+                    continue;
+                }
+                Node temp = current.child;
+                while (temp.next != null)
+                    temp = temp.next;
+                temp.next = current.next;  // Connect child tail with current.next, if it is not null
+                if (current.next != null) current.next.prev = temp; // if current.next is not null then change the prev of current.next to temp
+                // Connect node with current.child, and remove current.child make it null
+                current.next = current.child;
+                current.child.prev = current;
+                current.child = null;
+            }
+            return head;
+        }
+    }
+
+    class Solution1 {
+        public Node flatten(Node head) {
+            return dfs(head);
+        }
+
+        Node dfs(Node node){
+            if(node == null) return null;
+            Node next = node.next;
+            Node child = dfs(node.child);
+            node.child = null;
+            if(child!=null){
+                node.next = child;
+                child.prev = node;
+                while(child.next!=null) child = child.next;
+                child.next = dfs(next);
+                if(child.next!=null) child.next.prev = child;
+            } else {
+                node.next = dfs(next);
+            }
+
+            return node;
+        }
+    }
 
 }
