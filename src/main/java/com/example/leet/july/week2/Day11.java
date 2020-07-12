2b78700fa44d691611432be40fd2219253b77dd4
Amh <abaams78@gmail.com>
2020-07-11 22:53:01 -0500

    subsets

---
+++ src/main/java/com/example/leet/july/week2/Day11.java
@@ -0,0 +1,26 @@
+package com.example.leet.july.week2;
+
+/**
+ *  Subsets
+ * Given a set of distinct integers, nums, return all possible subsets (the power set).
+ *
+ * Note: The solution set must not contain duplicate subsets.
+ *
+ * Example:
+ *
+ * Input: nums = [1,2,3]
+ * Output:
+ * [
+ *   [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ */
+public class Day11 {
+
+}
