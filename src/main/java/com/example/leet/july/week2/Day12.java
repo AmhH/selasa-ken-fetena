2e9de8b21876373c906830998e0252aa5fa0b8f7
Amh <abaams78@gmail.com>
2020-07-12 20:10:49 -0500

    Reverse bits soln

--- src/main/java/com/example/leet/july/week2/Day12.java
+++ src/main/java/com/example/leet/july/week2/Day12.java
@@ -1,5 +1,7 @@
 package com.example.leet.july.week2;
 
+import java.util.Map;
+
 /**
  * Reverse Bits
  * Reverse bits of a given 32 bits unsigned integer.
@@ -34,4 +36,31 @@ package com.example.leet.july.week2;
  * If this function is called many times, how would you optimize it?
  */
 public class Day12 {
+    // you need treat n as an unsigned value
+    public static int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans = ans | (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseBits(43261596));
+    }
+
+    public class Solution {
+        // you need treat n as an unsigned value
+        public int reverseBits(int n) {
+            n = (n >>> 16) | (n << 16);
+            // n = ((n & 0xffff0000) >>> 16) | ((n & 0x0000ffff) << 16);
+            n = ((n & 0xff00ff00) >>> 8) | ((n & 0x00ff00ff) << 8);
+            n = ((n & 0xf0f0f0f0) >>> 4) | ((n & 0x0f0f0f0f) << 4);
+            n = ((n & 0xcccccccc) >>> 2) | ((n & 0x33333333) << 2);
+            n = ((n & 0xaaaaaaaa) >>> 1) | ((n & 0x55555555) << 1);
+            return n;
+        }
+    }
 }
