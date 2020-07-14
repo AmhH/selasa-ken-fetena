2d7358c77d8adbc12d2cfe85a653b6caa560b6fc
Amh <abaams78@gmail.com>
2020-07-13 21:29:33 -0500

    tree node util

---
+++ src/main/java/com/example/leet/util/TreeNode.java
@@ -0,0 +1,48 @@
+package com.example.leet.util;
+
+/**
+ * Definition for a binary tree node.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static TreeNode createTreeFromArray(int[] array) {
+        if (array.length > 0) {
+            TreeNode root = new TreeNode(array[0]);
+            java.util.Queue<TreeNode> queue = new java.util.LinkedList<TreeNode>();
+            queue.add(root);
+            boolean done = false;
+            int i = 1;
+            while (!done) {
+                TreeNode r = (TreeNode) queue.element();
+                if (r.left == null) {
+                    r.left = new TreeNode(array[i]);
+                    i++;
+                    queue.add(r.left);
+                } else if (r.right == null) {
+                    r.right = new TreeNode(array[i]);
+                    i++;
+                    queue.add(r.right);
+                } else {
+                    queue.remove();
+                }
+                if (i == array.length) {
+                    done = true;
+                }
+            }
+            return root;
+        } else {
+            return null;
+        }
+    }
+
+}
