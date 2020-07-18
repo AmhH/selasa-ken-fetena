08b062ef611ca1a0a0ded2f5f174afce28752cd5
Amh <abaams78@gmail.com>
2020-07-17 21:09:19 -0500

    top k frequent elements soln2

--- src/main/java/com/example/leet/july/week3/Day17.java
+++ src/main/java/com/example/leet/july/week3/Day17.java
@@ -24,6 +24,9 @@ import java.util.*;
 public class Day17 {
 
     public static int[] topKFrequent(int[] nums, int k) {
+        if (k == nums.length) {
+            return nums;
+        }
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i : nums){
             map.put(i, map.getOrDefault(i, 0) + 1);
@@ -42,5 +45,38 @@ public class Day17 {
         System.out.println(Arrays.toString(topKFrequent(new int[]{1,1,1,2,2,3}, 2)));
         System.out.println(Arrays.toString(topKFrequent(new int[]{1,1,1,2,2,3}, 1)));
         System.out.println(Arrays.toString(topKFrequent(new int[]{1}, 1)));
+    }
+
+    public int[] topKFrequent1(int[] nums, int k) {
+        // O(1) time
+        if (k == nums.length) {
+            return nums;
+        }
+
+        // 1. build hash map : character and how often it appears
+        // O(N) time
+        Map<Integer, Integer> count = new HashMap();
+        for (int n: nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        // init heap 'the less frequent element first' -> min heap
+        Queue<Integer> heap = new PriorityQueue<>(
+                (n1, n2) -> count.get(n1) - count.get(n2));
+
+        // 2. keep k top frequent elements in the heap
+        // O(N log k) < O(N log N) time
+        for (int n: count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) heap.poll();//when inserted min frequency will be at the top
+        }
+
+        // 3. build an output array
+        // O(k log k) time
+        int[] top = new int[k];
+        for(int i = k - 1; i >= 0; --i) {
+            top[i] = heap.poll();
+        }
+        return top;
     }
 }
