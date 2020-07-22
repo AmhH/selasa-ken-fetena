008e3f563ca3f12f1bbe4e58ee1a323e7da3fbba
Amh <abaams78@gmail.com>
2020-07-21 19:26:54 -0500

    Word search soln1

--- src/main/java/com/example/leet/july/week3/Day21.java
+++ src/main/java/com/example/leet/july/week3/Day21.java
@@ -85,5 +85,43 @@ public class Day21 {
                 {'B', 'C', 'D'}};
         System.out.println(exist(board1, "AAB"));
     }
+    class Solution {
+        //time : O(m*n*4^l) l is the word's length
+        public boolean exist(char[][] board, String word) {
+            int m = board.length;
+            int n = board[0].length;
+            char[] array = word.toCharArray();
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (board[i][j] == array[0] &&
+                            dfs(board, i, j, m, n, 0, array)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 
+        private boolean dfs(char[][] board, int i, int j,
+                            int m, int n, int cur, char[] word) {
+            if (cur == word.length) {
+                return true;
+            }
+
+            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[cur]) {
+                return false;
+            }
+
+            char tmp = board[i][j];
+            board[i][j] = '#';
+            boolean ans = dfs(board, i - 1, j, m, n, cur + 1, word) ||
+                    dfs(board, i + 1, j, m, n, cur + 1, word) ||
+                    dfs(board, i, j - 1, m, n, cur + 1, word) ||
+                    dfs(board, i, j + 1, m, n, cur + 1, word);
+            board[i][j] = tmp;
+            return ans;
+
+
+        }
+    }
 }
