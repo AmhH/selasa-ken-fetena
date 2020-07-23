a45713ffc8c317db2fb00da74e363a4941212abb
Amh <abaams78@gmail.com>
2020-07-22 22:09:08 -0500

    Binary tree ZigZag level order traversal soln

--- src/main/java/com/example/leet/july/week4/Day22.java
+++ src/main/java/com/example/leet/july/week4/Day22.java
@@ -1,5 +1,12 @@
 package com.example.leet.july.week4;
 
+import com.example.leet.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * Binary Tree Zigzag Level Order Traversal
  * Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then
@@ -20,4 +27,43 @@ package com.example.leet.july.week4;
  * ]
  */
 public class Day22 {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null)
+            return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean inOrder = true;
+        int size = 1;
+        while (!q.isEmpty()){
+            LinkedList<Integer> temp = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if(inOrder){
+                    temp.addLast(node.val);
+                }else {
+                    temp.addFirst(node.val);
+                }
+                if(node.left !=  null)
+                    q.add(node.left);
+                if(node.right !=  null)
+                    q.add(node.right);
+            }
+
+
+            result.add(temp);
+            size = q.size();
+            inOrder = !inOrder;
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = TreeNode.createTreeFromArray(new int[]{3, 9, 20, -1, -1, 15, 7});
+        System.out.println(node);
+    }
+
+
 }
