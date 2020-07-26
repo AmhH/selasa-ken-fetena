95ffde74b5bf33d18588c4bdcc16b75c56e39901
Amh <abaams78@gmail.com>
2020-07-25 22:49:05 -0500

    Find Minimum in Rotated Sorted Array II soln

--- src/main/java/com/example/leet/july/week4/Day25.java
+++ src/main/java/com/example/leet/july/week4/Day25.java
@@ -24,4 +24,50 @@ package com.example.leet.july.week4;
  * Would allow duplicates affect the run-time complexity? How and why?
  */
 public class Day25 {
+
+    public static int findMin(int[] nums) {
+        int min = nums[0];
+        for (int val : nums) {
+            if (min > val) {
+                min = val;
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMin(new int[]{1,3,5}));
+        System.out.println(findMin(new int[]{2,2,2,0,1}));
+        System.out.println(findMin(new int[]{4,5,6,7,0,1,2}));
+        System.out.println(findMin(new int[]{1,1}));
+    }
+
+    public int findMin1(int[] nums) {
+        int lo =0;
+        int n = nums.length;
+        int hi = n-1;
+        int min = nums[lo];
+        while(lo<hi){
+            while(lo <hi && lo <n-2 && nums[lo]==nums[lo+1] ){
+                lo = lo+1;
+            }
+            while(lo<hi && hi >=1 && nums[hi]== nums[hi-1]){
+                hi = hi-1;
+            }
+            int mid = lo +(hi-lo)/2;
+            if(mid-1>=0 && mid+1<=n-1 && nums[mid]<nums[mid-1]&& nums[mid]>nums[mid+1]){
+                return nums[mid];
+            }
+            if(nums[mid]<nums[hi]){
+                hi = mid;
+            } else {
+                lo = mid+1;
+            }
+
+
+        }
+        return nums[lo];
+
+    }
 }
