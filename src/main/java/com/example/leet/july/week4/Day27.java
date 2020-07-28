3d24c6e52d599196015b3aba7c2498fe3a445d55
Amh <abaams78@gmail.com>
2020-07-27 22:06:41 -0500

    construct binary tree from in order and post order traversal

---
+++ src/main/java/com/example/leet/july/week4/Day27.java
@@ -0,0 +1,29 @@
+package com.example.leet.july.week4;
+
+import com.example.leet.util.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * Construct Binary Tree from Inorder and Postorder Traversal
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ *
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ *
+ * For example, given
+ *
+ * inorder = [9,3,15,20,7]
+ * postorder = [9,15,7,20,3]
+ * Return the following binary tree:
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ */
+public class Day27 {
+
+
+}
