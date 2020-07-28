2196d666aae6c8db03bd790670f8147f253f5b49
Amh <abaams78@gmail.com>
2020-07-27 22:06:54 -0500

    construct binary tree from in order and post order traversal soln

--- src/main/java/com/example/leet/july/week4/Day27.java
+++ src/main/java/com/example/leet/july/week4/Day27.java
@@ -25,5 +25,61 @@ import java.util.Stack;
  */
 public class Day27 {
 
+    public static TreeNode buildTree(int[] inorder, int[] postorder) {
 
+        return buildTree(inorder, inorder.length - 1, 0, postorder, postorder.length - 1);
+    }
+
+    private static TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart) {
+        if(postStart < 0 || inEnd > inStart)
+            return null;
+
+        TreeNode root = new TreeNode(postorder[postStart]);
+        int index = inStart;
+        for (int i = inStart; i >= inEnd; i--) {
+            if(inorder[i] == postorder[postStart]){
+                index = i;
+                break;
+            }
+        }
+
+        root.right = buildTree(inorder, inStart, index + 1, postorder, postStart -1);
+        root.left = buildTree(inorder, index - 1, inEnd,  postorder, postStart - (inStart - index) -1);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(buildTree(new int[]{9,3,15,20,7}, new int[]{9,15,7,20,3}));
+    }
+
+    public TreeNode buildTreeIetrative(int[] inorder, int[] postorder) {
+        if (inorder == null || inorder.length < 1) return null;
+        int i = inorder.length - 1;
+        int p = i;
+        TreeNode node;
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        p--;
+
+        while (true) {
+            if (inorder[i] == stack.peek().val) { // inorder[i] is on top of stack, pop stack to get its parent to get to left side
+                if (--i < 0) break;
+                node = stack.pop();
+                if (!stack.isEmpty() && inorder[i] == stack.peek().val) {// continue pop stack to get to left side
+                    continue;
+                }
+                node.left = new TreeNode(postorder[p]);
+                stack.push(node.left);
+            } else { // inorder[i] is not on top of stack, postorder[p] must be right child
+                node = new TreeNode(postorder[p]);
+                stack.peek().right = node;
+                stack.push(node);
+            }
+            p--;
+        }
+
+        return root;
+    }
 }
