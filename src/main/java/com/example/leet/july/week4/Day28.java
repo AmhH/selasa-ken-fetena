a180a0bd2c22dda8458875d47f47476d5fdfd655
Amh <abaams78@gmail.com>
2020-07-28 20:52:33 -0500

    Task Scheduler soln

--- src/main/java/com/example/leet/july/week4/Day28.java
+++ src/main/java/com/example/leet/july/week4/Day28.java
@@ -45,4 +45,34 @@ package com.example.leet.july.week4;
  * The integer n is in the range [0, 100].
  */
 public class Day28 {
+
+    public static int leastInterval(char[] tasks, int n) {
+        int[] counter = new int[26];
+        int max = 0;
+        int maxCount = 0;
+        for(char task : tasks) {
+            counter[task - 'A']++;
+            if(max == counter[task - 'A']) {
+                maxCount++;
+            }
+            else if(max < counter[task - 'A']) {
+                max = counter[task - 'A'];
+                maxCount = 1;
+            }
+        }
+
+        int partCount = max - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.length - max * maxCount;
+        int idles = Math.max(0, emptySlots - availableTasks);
+
+        return tasks.length + idles;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(leastInterval(new char[]{'A','A','A','B','B','B'}, 2));
+        System.out.println(leastInterval(new char[]{'A','A','A','B','B','B'}, 0));
+        System.out.println(leastInterval(new char[]{'A','A','A','A','A','A','B','C','D','E','F','G'}, 2));
+    }
 }
