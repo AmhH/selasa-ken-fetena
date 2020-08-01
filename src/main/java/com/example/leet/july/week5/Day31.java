7d3fbe0df47fa29dbde11d064e6b1590dfaa76c4
Amh <abaams78@gmail.com>
2020-07-31 20:26:04 -0500

    Climbing stairs soln 2

--- src/main/java/com/example/leet/july/week5/Day31.java
+++ src/main/java/com/example/leet/july/week5/Day31.java
@@ -57,4 +57,10 @@ public class Day31 {
         }
         return result;
     }
+
+    public int climbStairs2(int n) {
+        double sqrt5=Math.sqrt(5);
+        double fibn =Math.pow((1+sqrt5)/2,n+1)-Math.pow((1-sqrt5)/2,n+1);
+        return (int)(fibn/sqrt5);
+    }
 }
