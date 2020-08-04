1c0a292726d91d8b2fd01dcf20de1aa461a8513c
Amh <abaams78@gmail.com>
2020-08-03 20:43:42 -0500

    Valid Palindrome soln

--- src/main/java/com/example/leet/august/week1/Day3.java
+++ src/main/java/com/example/leet/august/week1/Day3.java
@@ -20,7 +20,78 @@ package com.example.leet.august.week1;
  * s consists only of printable ASCII characters.
  */
 public class Day3 {
+    public static boolean isPalindrome(String s) {
+        int start = 0, end = s.length() - 1;
+        while (start < end){
+            if (!Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            } else if (!Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            } else if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));     //true
+        System.out.println(isPalindrome("race a car"));     //false
+        System.out.println(isPalindrome("mom"));        //true
+        System.out.println(isPalindrome("Anna"));       //true
+        System.out.println(isPalindrome("   "));        //true
+        System.out.println(isPalindrome(""));           //true
+        System.out.println(isPalindrome(".,"));         //true
+        System.out.println(isPalindrome("."));          //true
+        System.out.println(isPalindrome1("."));          //true
+    }
+
+    public static boolean isPalindrome1(String s) {
+        if (s.length() == 0) return true;
+        //initialize two pointers
+        int i = 0;
+        int j = s.length()-1;
+
+        while (i < j) {
+
+            char first = s.charAt(i);
+            char second = s.charAt(j);
+
+            int aDifference = first - 'a';
+            int ADifference = first - 'A';
+            int digitDifference = first - '0';
+
+            int aDifference1 = second - 'a';
+            int ADifference1 = second - 'A';
+            int digitDifference1 = second - '0';
+
+            if ((aDifference < 0 || aDifference >= 26) && (ADifference < 0 || ADifference >=26) && (digitDifference <0 || digitDifference >9)) {
+                i++;
+                continue;
+            }
+
+            if ((aDifference1 < 0 || aDifference1 >= 26) && (ADifference1 < 0 || ADifference1 >=26) && (digitDifference1 <0 || digitDifference1 >9)) {
+                j--;
+                continue;
+            }
+
+            if (aDifference >= 0 && aDifference < 26) {
+                if (aDifference != aDifference1 && aDifference != ADifference1) return false;
+            } else if (ADifference >= 0 && ADifference < 26) {
+                if (ADifference != aDifference1 && ADifference != ADifference1) return false;
+            } else {
+                if (digitDifference != digitDifference1) return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
 
 }
 
