8b8649b64361b2e35b4b54fbb3eb74afb77a1314
Amh <abaams78@gmail.com>
2020-08-05 23:15:44 -0500

    Add and search word

--- src/main/java/com/example/leet/august/week1/Day4.java
+++ src/main/java/com/example/leet/august/week1/Day4.java
@@ -21,10 +21,18 @@ public class Day4 {
         return (logValue % 1) == 0 ;
     }
 
+    public static boolean isPowerOfFour1(int num) {
+        return num > 0 &&
+                (num & (num - 1)) == 0 &&
+                (num & 0x55555555) != 0;
+    }
+
     public static void main(String[] args) {
         System.out.println(isPowerOfFour(16));
         System.out.println(isPowerOfFour(5));
         System.out.println(isPowerOfFour(32));
         System.out.println(isPowerOfFour(64));
+        System.out.println(isPowerOfFour1(64));
+        System.out.println(0x55555555);
     }
 }
