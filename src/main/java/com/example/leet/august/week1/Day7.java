62d8a5746d3e6a046fcb4c6a89b998c247d65499
Amh <abaams78@gmail.com>
2020-08-07 22:55:27 -0500

    vertical order traversal soln

--- src/main/java/com/example/leet/august/week1/Day7.java
+++ src/main/java/com/example/leet/august/week1/Day7.java
@@ -1,5 +1,12 @@
 package com.example.leet.august.week1;
 
+import com.example.leet.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
 /**
  * Vertical Order Traversal of a Binary Tree
  * Given a binary tree, return the vertical order traversal of its nodes values.
@@ -47,4 +54,35 @@ package com.example.leet.august.week1;
  * Each node's value will be between 0 and 1000.
  */
 public class Day7 {
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        dfs(root, 0, 0, map);
+        List<List<Integer>> list = new ArrayList<>();
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            list.add(new ArrayList<>());
+            for (PriorityQueue<Integer> nodes : ys.values()) {
+                while (!nodes.isEmpty()) {
+                    list.get(list.size() - 1).add(nodes.poll());
+                }
+            }
+        }
+        return list;
+    }
+    private void dfs(TreeNode root, int x, int y, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map) {
+        if (root == null) {
+            return;
+        }
+        if (!map.containsKey(x)) {
+            map.put(x, new TreeMap<>());
+        }
+        if (!map.get(x).containsKey(y)) {
+            map.get(x).put(y, new PriorityQueue<>());
+        }
+        map.get(x).get(y).offer(root.val);
+        dfs(root.left, x - 1, y + 1, map);
+        dfs(root.right, x + 1, y + 1, map);
+    }
+
+
 }
