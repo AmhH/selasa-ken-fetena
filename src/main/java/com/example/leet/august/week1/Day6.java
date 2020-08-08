be3f08a5f809a963ac6df9dee33a1b28057f7124
Amh <abaams78@gmail.com>
2020-08-07 22:15:22 -0500

    find all duplicates in array soln

--- src/main/java/com/example/leet/august/week1/Day6.java
+++ src/main/java/com/example/leet/august/week1/Day6.java
@@ -20,4 +20,33 @@ import java.util.List;
  */
 public class Day6 {
 
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index] < 0)
+                res.add(Math.abs(index+1));
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findDuplicates(new int[]{4,3,2,7,8,2,3,1}));
+        System.out.println(findDuplicates1(new int[]{4,3,2,7,8,2,3,1}));
+    }
+
+    public static List<Integer> findDuplicates1(int[] nums) {
+        int[] arr = new int[nums.length + 1];
+
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            arr[num]++;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 1)
+                list.add(i);
+        }
+        return list;
+    }
 }
