a47301c55be06d4f46ec7a28cdf6ec951e3d9a7d
Amh <abaams78@gmail.com>
2020-08-12 14:41:26 -0500

    Pascal's Triangle II

---
+++ src/main/java/com/example/leet/august/week2/Day12.java
@@ -0,0 +1,28 @@
+package com.example.leet.august.week2;
+
+/**
+ * Pascal's Triangle II
+ * Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+ *
+ * Note that the row index starts from 0.
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it.
+ *
+ * Example:
+ *
+ * Input: 3
+ *      1
+ *     1 1
+ *    1 2 1
+ *   1 3 3 1
+ *  1 4 6 4 1
+ * 1 5 10 10 5 1
+ * Output: [1,3,3,1]
+ *
+ * Follow up:
+ *
+ * Could you optimize your algorithm to use only O(k) extra space?
+ */
+public class Day12 {
+
+}
