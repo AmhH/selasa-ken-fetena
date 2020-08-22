1fc6d5757b01ff476b501b54016f9c9dbf748c66
Amh <abaams78@gmail.com>
2020-08-21 21:34:39 -0500

    Sort Array by Parity soln

--- src/main/java/com/example/leet/august/week3/Day21.java
+++ src/main/java/com/example/leet/august/week3/Day21.java
@@ -24,5 +24,53 @@ import java.util.Arrays;
  * 0 <= A[i] <= 5000
  */
 public class Day21 {
+    public static int[] sortArrayByParity(int[] A) {
+        if(null == A || A.length < 2)
+            return A;
+        int even = 0;
+        int odd = A.length - 1;
+        int[] res = new int[A.length];
+        for(int i : A){
+            if(i%2 == 0){
+                res[even++] = i;
+            }else{
+                res[odd--] = i;
+            }
+        }
+        return res;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortArrayByParity(new int[]{3,1,2,4})));
+        System.out.println(Arrays.toString(sortArrayByParity1(new int[]{5,3,1,2,4})));
+    }
+
+    public static int[] sortArrayByParity1(int[] A) {
+        int indx = 0, val;
+        for (int i = 0; i < A.length; i++) {
+            if ((A[i] & 1) != 1) {
+                val = A[indx];
+                A[indx] = A[i];
+                A[i] = val;
+                indx++;
+            }
+        }
+        return A;
+    }
+
+    public int[] sortArrayByParity2(int[] A) {
+        int i = 0, j = A.length - 1;
+        while (i < j) {
+            if (A[i] % 2 > A[j] % 2) {
+                int tmp = A[i];
+                A[i] = A[j];
+                A[j] = tmp;
+            }
+
+            if (A[i] % 2 == 0) i++;
+            if (A[j] % 2 == 1) j--;
+        }
+
+        return A;
+    }
 }
