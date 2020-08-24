b138b41d320196f7509aaf04fffb548b2e39f47e
Amh <abaams78@gmail.com>
2020-08-23 19:22:35 -0500

    Stream of character soln

--- src/main/java/com/example/leet/august/week4/Day23.java
+++ src/main/java/com/example/leet/august/week4/Day23.java
@@ -37,14 +37,47 @@ package com.example.leet.august.week4;
  * Put the words into a trie, and manage a set of pointers within that trie.
  */
 public class Day23 {
-    class StreamChecker {
+    //Store the words in the trie with reverse order, and check the query string from the end
+    static class StreamChecker {
+
+        class TrieNode {
+            boolean isWord;
+            TrieNode[] next = new TrieNode[26];
+        }
+
+        TrieNode root = new TrieNode();
+        StringBuilder sb = new StringBuilder();
 
         public StreamChecker(String[] words) {
-
+            createTrie(words);
         }
 
         public boolean query(char letter) {
+            sb.append(letter);
+            TrieNode node = root;
+            for (int i = sb.length() - 1; i >= 0 && node != null; i--) {
+                char c = sb.charAt(i);
+                node = node.next[c - 'a'];
+                if (node != null && node.isWord) {
+                    return true;
+                }
+            }
+            return false;
+        }
 
+        private void createTrie(String[] words) {
+            for (String s : words) {
+                TrieNode node = root;
+                int len = s.length();
+                for (int i = len - 1; i >= 0; i--) {
+                    char c = s.charAt(i);
+                    if (node.next[c - 'a'] == null) {
+                        node.next[c - 'a'] = new TrieNode();
+                    }
+                    node = node.next[c - 'a'];
+                }
+                node.isWord = true;
+            }
         }
     }
     /**
@@ -53,6 +86,18 @@ public class Day23 {
      * boolean param_1 = obj.query(letter);
      */
     public static void main(String[] args) {
-
+        StreamChecker streamChecker = new StreamChecker(new String[]{"cd", "f", "kl"}); // init the dictionary.
+        System.out.println(streamChecker.query('a'));          // return false
+        System.out.println(streamChecker.query('b'));          // return false
+        System.out.println(streamChecker.query('c'));          // return false
+        System.out.println(streamChecker.query('d'));          // return true, because 'cd' is in the wordlist
+        System.out.println(streamChecker.query('e'));          // return false
+        System.out.println(streamChecker.query('f'));          // return true, because 'f' is in the wordlist
+        System.out.println(streamChecker.query('g'));          // return false
+        System.out.println(streamChecker.query('h'));          // return false
+        System.out.println(streamChecker.query('i'));          // return false
+        System.out.println(streamChecker.query('j'));          // return false
+        System.out.println(streamChecker.query('k'));          // return false
+        System.out.println(streamChecker.query('l'));          // return true, because 'kl' is in the wordlist
     }
 }
