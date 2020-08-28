432fbd6f5c4171af2ca48321398c1d4a16d48316
Amh <abaams78@gmail.com>
2020-08-27 22:19:13 -0500

    Find right interval soln

--- src/main/java/com/example/leet/august/week4/Day27.java
+++ src/main/java/com/example/leet/august/week4/Day27.java
@@ -1,5 +1,6 @@
 package com.example.leet.august.week4;
 
+import java.util.Arrays;
 import java.util.TreeMap;
 
 /**
@@ -16,31 +17,22 @@ import java.util.TreeMap;
  * You may assume the interval's end point is always bigger than its start point.
  * You may assume none of these intervals have the same start point.
  *
- *
  * Example 1:
- *
  * Input: [ [1,2] ]
- *
  * Output: [-1]
  *
  * Explanation: There is only one interval in the collection, so it outputs -1.
  *
- *
  * Example 2:
- *
  * Input: [ [3,4], [2,3], [1,2] ]
- *
  * Output: [-1, 0, 1]
  *
  * Explanation: There is no satisfied "right" interval for [3,4].
  * For [2,3], the interval [3,4] has minimum-"right" start point;
  * For [1,2], the interval [2,3] has minimum-"right" start point.
  *
- *
  * Example 3:
- *
  * Input: [ [1,4], [2,3], [3,4] ]
- *
  * Output: [-1, 2, -1]
  *
  * Explanation: There is no satisfied "right" interval for [1,4] and [3,4].
@@ -49,5 +41,59 @@ import java.util.TreeMap;
  * signature.
  */
 public class Day27 {
+    public static int[] findRightInterval(int[][] intervals) {
+        int n = intervals.length;
+        if (n == 1)
+            return new int[]{-1};
 
+        TreeMap<Integer, Integer> map = new TreeMap();
+        for (int i = 0; i < n; i++) {
+            map.put(intervals[i][0], i);
+        }
+
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            Integer key = map.ceilingKey(intervals[i][1]);
+            result[i] = key == null ? -1 : map.get(key);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(findRightInterval(new int[][]{{1,2}})));
+        System.out.println(Arrays.toString(findRightInterval(new int[][]{{3,4},{2,3},{1,2}})));
+        System.out.println(Arrays.toString(findRightInterval2(new int[][]{{1,4},{2,3},{3,4}})));
+    }
+
+    public static int[] findRightInterval2(int[][] intervals) {
+        int minStartPoint = Integer.MAX_VALUE;
+        int maxEndPoint = Integer.MIN_VALUE;
+
+        for (int i = 0; i < intervals.length; i++) {
+            minStartPoint = Math.min(minStartPoint, intervals[i][0]);
+            maxEndPoint = Math.max(maxEndPoint, intervals[i][1]);
+        }
+
+        int[] buckets = new int[maxEndPoint - minStartPoint + 1];
+        Arrays.fill(buckets, -1);
+
+        for (int i = 0; i < intervals.length; i++) {
+            buckets[intervals[i][0] - minStartPoint] = i;
+        }
+
+        for (int i = buckets.length - 2; i > -1; i--) {
+            if (buckets[i] == -1) {
+                buckets[i] = buckets[i + 1];
+            }
+        }
+
+        int[] rightIntervals = new int[intervals.length];
+
+        for (int i = 0; i < intervals.length; i++) {
+            rightIntervals[i] = buckets[intervals[i][1] - minStartPoint];
+        }
+
+        return rightIntervals;
+    }
 }
