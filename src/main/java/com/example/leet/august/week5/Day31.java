5a41194aad8da3a21f2b2d2d8d4468e84869d2c3
Amh <abaams78@gmail.com>
2020-08-31 21:14:35 -0500

    Delete Node in a BST soln

--- src/main/java/com/example/leet/august/week5/Day31.java
+++ src/main/java/com/example/leet/august/week5/Day31.java
@@ -46,5 +46,38 @@ import java.util.Queue;
  *     4   7
  */
 public class Day31 {
+    public static TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return null;
+        }
+        if(key < root.val){
+            root.left = deleteNode(root.left, key);
+        }else if(key > root.val){
+            root.right = deleteNode(root.right, key);
+        }else{
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
 
+            TreeNode minNode = findMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, root.val);
+        }
+        return root;
+    }
+
+    private static TreeNode findMin(TreeNode node){
+        while(node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.createTreeFromArray(new Integer[]{5,3,6,2,4,null,7});
+        System.out.println(root);
+    }
 }
