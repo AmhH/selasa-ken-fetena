d053f692fca9cc8258ba554b316d4f4314ca83ed
Amh <abaams78@gmail.com>
2020-09-03 23:13:04 -0500

    Repeated substring pattern soln

--- src/main/java/com/example/leet/september/week1/Day3.java
+++ src/main/java/com/example/leet/september/week1/Day3.java
@@ -23,11 +23,45 @@ package com.example.leet.september.week1;
  */
 public class Day3 {
 
-    public boolean repeatedSubstringPattern(String s) {
-
+    public static boolean repeatedSubstringPattern(String str) {
+        String s = str + str;
+        return s.substring(1, s.length() - 1).contains(str);
     }
 
     public static void main(String[] args) {
+        System.out.println(repeatedSubstringPattern("abab"));
+        System.out.println(repeatedSubstringPattern("aba"));
+        System.out.println(repeatedSubstringPattern("abcabcabcabc"));
+        System.out.println(repeatedSubstringPattern2("abaabaaba"));
+    }
 
+    public static boolean repeatedSubstringPattern2(String str) {
+        //This is the kmp issue
+        int[] prefix = kmp(str);
+        int len = prefix[str.length()-1];
+        int n = str.length();
+        return (len > 0 && n%(n-len) == 0);
+    }
+    private static int[] kmp(String s){
+        int len = s.length();
+        int[] res = new int[len];
+        char[] ch = s.toCharArray();
+        int i = 0, j = 1;
+        res[0] = 0;
+        while(i < ch.length && j < ch.length){
+            if(ch[j] == ch[i]){
+                res[j] = i+1;
+                i++;
+                j++;
+            }else{
+                if(i == 0){
+                    res[j] = 0;
+                    j++;
+                }else{
+                    i = res[i-1];
+                }
+            }
+        }
+        return res;
     }
 }
