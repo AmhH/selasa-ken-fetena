f38f577c61fde2a4e787facd2fce107237d27eaf
Amh <abaams78@gmail.com>
2020-09-13 01:34:42 -0500

    combination sum II soln

--- src/main/java/com/example/leet/september/week2/Day12.java
+++ src/main/java/com/example/leet/september/week2/Day12.java
@@ -1,5 +1,7 @@
 package com.example.leet.september.week2;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -21,8 +23,33 @@ import java.util.List;
  * Output: [[1,2,6], [1,3,5], [2,3,4]]
  */
 public class Day12 {
-    public List<List<Integer>> combinationSum3(int k, int n) {
+    protected void backtrack(int remain, int k,
+                             LinkedList<Integer> comb, int next_start,
+                             List<List<Integer>> results) {
 
+        if (remain == 0 && comb.size() == k) {
+            // Note: it's important to make a deep copy here.
+            results.add(new ArrayList<Integer>(comb));
+            return;
+        } else if (remain < 0 || comb.size() == k) {
+            // Exceed the scope, no need to explore further.
+            return;
+        }
+
+        // Iterate through the reduced list of candidates.
+        for (int i = next_start; i < 9; ++i) {
+            comb.add(i + 1);
+            this.backtrack(remain - i - 1, k, comb, i + 1, results);
+            comb.removeLast();
+        }
+    }
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        LinkedList<Integer> comb = new LinkedList<Integer>();
+
+        this.backtrack(n, k, comb, 0, results);
+        return results;
     }
 
     public static void main(String[] args) {
