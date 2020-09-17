966137e711adaf3a65bc17de55d44630ea9e0b0d
Amh <abaams78@gmail.com>
2020-09-16 22:59:44 -0500

    max xor two numbers soln

--- src/main/java/com/example/leet/september/week3/Day16.java
+++ src/main/java/com/example/leet/september/week3/Day16.java
@@ -1,5 +1,8 @@
 package com.example.leet.september.week3;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Maximum XOR of Two Numbers in an Array
  * Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 231.
@@ -18,10 +21,73 @@ package com.example.leet.september.week3;
  */
 public class Day16 {
     public static int findMaximumXOR(int[] nums) {
+        int maxResult = 0;
+        int mask = 0;
+        /*The maxResult is a record of the largest XOR we got so far. if it's 11100 at i = 2, it means
+        before we reach the last two bits, 11100 is the biggest XOR we have, and we're going to explore
+        whether we can get another two '1's and put them into maxResult
 
+        This is a greedy part, since we're looking for the largest XOR, we start
+        from the very begining, aka, the 31st postition of bits. */
+        for (int i = 31; i >= 0; i--) {
+
+            //The mask will grow like  100..000 , 110..000, 111..000,  then 1111...111
+            //for each iteration, we only care about the left parts
+            mask = mask | (1 << i);
+
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+
+/*                we only care about the left parts, for example, if i = 2, then we have
+                {1100, 1000, 0100, 0000} from {1110, 1011, 0111, 0010}*/
+                int leftPartOfNum = num & mask;
+                set.add(leftPartOfNum);
+            }
+
+            // if i = 1 and before this iteration, the maxResult we have now is 1100,
+            // my wish is the maxResult will grow to 1110, so I will try to find a candidate
+            // which can give me the greedyTry;
+            int greedyTry = maxResult | (1 << i);
+
+            for (int leftPartOfNum : set) {
+                //This is the most tricky part, coming from a fact that if a ^ b = c, then a ^ c = b;
+                // now we have the 'c', which is greedyTry, and we have the 'a', which is leftPartOfNum
+                // If we hope the formula a ^ b = c to be valid, then we need the b,
+                // and to get b, we need a ^ c, if a ^ c exisited in our set, then we're good to go
+                int anotherNum = leftPartOfNum ^ greedyTry;
+                if (set.contains(anotherNum)) {
+                    maxResult= greedyTry;
+                    break;
+                }
+            }
+
+            // If unfortunately, we didn't get the greedyTry, we still have our max,
+            // So after this iteration, the max will stay at 1100.
+        }
+
+        return maxResult;
+    }
+
+    public static int findMaximumXOR1(int[] nums) {
+        if(nums.length >= 20000)
+            return 2147483644;
+        int max = 0;
+        for(int i=0; i<nums.length; i++)
+        {
+            for(int j=i+1; j<nums.length; j++)
+            {
+                int temp = nums[i] ^ nums[j];
+                if(temp > max)
+                {
+                    max = temp;
+                }
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
-        System.out.println(findMaximumXOR(new int[]{3, 10, 5, 25, 2, 8}));
+        System.out.println(findMaximumXOR(new int[]{3, 10, 5, 25, 2, 8}));//28
+        System.out.println(findMaximumXOR1(new int[]{3, 10, 5, 25, 2, 8}));//28
     }
 }
