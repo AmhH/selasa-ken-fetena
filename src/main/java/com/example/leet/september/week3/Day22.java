88d49ff1a7fef988177f769334c9206917ec4ad8
Amh <abaams78@gmail.com>
2020-09-22 17:17:27 -0500

    Majority Element II soln

--- src/main/java/com/example/leet/september/week3/Day22.java
+++ src/main/java/com/example/leet/september/week3/Day22.java
@@ -22,5 +22,47 @@ import java.util.List;
  * Do you have a better hint? Suggest it!
  */
 public class Day22 {
+    public static List<Integer> majorityElement(int[] nums) {
+        int c1 = 0;
+        int c2 = 0;
+        Integer candidate1 = null;
+        Integer candidate2 = null;
 
+        for (int i : nums){
+            if(null != candidate1 && candidate1.equals(i)){
+                c1++;
+            }else if(null != candidate2 && candidate2.equals(i)){
+                c2++;
+            }else if(c1 == 0){
+                candidate1 = i;
+                c1++;
+            }else if(c2 == 0){
+                candidate2 = i;
+                c2++;
+            }else {
+                c1--;
+                c2--;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        c1 = 0;
+        c2 = 0;
+        for (int i : nums){
+            if(null != candidate1 && candidate1.equals(i)) c1++;
+            if(null != candidate2 && candidate2.equals(i)) c2++;
+        }
+
+        int n = nums.length;
+        if (c1 > n/3) list.add(candidate1);
+        if (c2 > n/3) list.add(candidate2);
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[]{3,2,3}));//[3]
+        System.out.println(majorityElement(new int[]{1,1,1,3,3,2,2,2}));//[1,2]
+        System.out.println(majorityElement(new int[]{1,2}));//[1,2]
+    }
 }
