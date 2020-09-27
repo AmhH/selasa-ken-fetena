8b10832540b53241c4336a01059b4ebba33364dc
Amh <abaams78@gmail.com>
2020-09-26 23:05:20 -0500

    Teemo Attackin soln

--- src/main/java/com/example/leet/september/week4/Day26.java
+++ src/main/java/com/example/leet/september/week4/Day26.java
@@ -39,11 +39,17 @@ package com.example.leet.september.week4;
  */
 public class Day26 {
     public static int findPoisonedDuration(int[] timeSeries, int duration) {
+        int n = timeSeries.length;
+        if (n == 0) return 0;
 
+        int total = 0;
+        for(int i = 0; i < n - 1; ++i)
+            total += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+        return total + duration;
     }
 
     public static void main(String[] args) {
-        System.out.println(findPoisonedDuration(new int[]{1, 4}, 2));
-        System.out.println(findPoisonedDuration(new int[]{1, 2}, 2));
+        System.out.println(findPoisonedDuration(new int[]{1, 4}, 2));//4
+        System.out.println(findPoisonedDuration(new int[]{1, 2}, 2));//3
     }
 }
