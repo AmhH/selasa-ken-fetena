f56154ab6457e345a1bbed3d822d007eab88ed7d
Amh <abaams78@gmail.com>
2020-09-29 22:45:17 -0500

    Word break

---
+++ src/main/java/com/example/leet/september/week4/Day29.java
@@ -0,0 +1,40 @@
+package com.example.leet.september.week4;
+
+import java.util.List;
+
+/**
+ * Word Break
+ * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be
+ * segmented into a space-separated sequence of one or more dictionary words.
+ *
+ * Note:
+ *
+ * The same word in the dictionary may be reused multiple times in the segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ * Example 1:
+ *
+ * Input: s = "leetcode", wordDict = ["leet", "code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet code".
+ * Example 2:
+ *
+ * Input: s = "applepenapple", wordDict = ["apple", "pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+ *              Note that you are allowed to reuse a dictionary word.
+ * Example 3:
+ *
+ * Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output: false
+ */
+public class Day29 {
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
