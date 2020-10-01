5a21571efefc74840895f98cab395629b3700ac1
Amh <abaams78@gmail.com>
2020-09-30 22:20:50 -0500

    First Missing Positive soln

--- src/main/java/com/example/leet/september/week5/Day30.java
+++ src/main/java/com/example/leet/september/week5/Day30.java
@@ -1,5 +1,8 @@
 package com.example.leet.september.week5;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * First Missing Positive
  * Given an unsorted integer array, find the smallest missing positive integer.
@@ -28,4 +31,39 @@ package com.example.leet.september.week5;
  * Remember that O(2n) = O(n)
  */
 public class Day30 {
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0, n = nums.length;
+        while (i < n) {
+            // If the current value is in the range of (0,length) and it's not at its correct position,
+            // swap it to its correct position.
+            // Else just continue;
+            if (nums[i] >= 0 && nums[i] < n && nums[nums[i]] != nums[i])
+                swap(nums, i, nums[i]);
+            else
+                i++;
+        }
+        int k = 1;
+
+        // Check from k=1 to see whether each index and value can be corresponding.
+        while (k < n && nums[k] == k)
+            k++;
+
+        // If it breaks because of empty array or reaching the end. K must be the first missing number.
+        if (n == 0 || k < n)
+            return k;
+        else   // If k is hiding at position 0, K+1 is the number.
+            return nums[0] == k ? k + 1 : k;
+
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        nums[i] = nums[j];
+        nums[j] = j;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstMissingPositive(new int[]{1,2,0}));//3
+        System.out.println(firstMissingPositive(new int[]{3,4,-1,1}));//2
+        System.out.println(firstMissingPositive(new int[]{7,8,9,11,12}));//1
+    }
 }
