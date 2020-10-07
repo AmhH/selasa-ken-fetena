79728be022f835d0d9f19231c788d159685cd5a5
Amh <abaams78@gmail.com>
2020-10-06 23:15:42 -0500

    Insert into BST soln

--- src/main/java/com/example/leet/october/week1/Day6.java
+++ src/main/java/com/example/leet/october/week1/Day6.java
@@ -1,5 +1,10 @@
 package com.example.leet.october.week1;
 
+import com.example.leet.util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Insert into a Binary Search Tree
  * You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root
@@ -45,4 +50,41 @@ package com.example.leet.october.week1;
  * It's guaranteed that val does not exist in the original BST.
  */
 public class Day6 {
+
+    public static TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        } else if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(print(insertIntoBST(TreeNode.createTreeFromArray(new Integer[]{4,2,7,1,3}), 5)));
+        System.out.println(print(insertIntoBST(TreeNode.createTreeFromArray(new Integer[]{40,20,60,10,30,50,70}), 25)));
+        System.out.println(print(insertIntoBST(TreeNode.createTreeFromArray(new Integer[]{4,2,7,1,3}), 5)));
+    }
+
+    private static String print(TreeNode root) {
+        StringBuilder builder = new StringBuilder(root.val).append(" ");
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+        while (!q.isEmpty()){
+            TreeNode node = q.poll();
+            if(null != node.left){
+                builder.append(node.left.val).append(" ");
+                q.offer(node.left);
+            }
+            if(null != node.right){
+                builder.append(node.right.val).append(" ");
+                q.offer(node.right);
+            }
+        }
+
+        return builder.toString();
+    }
 }
