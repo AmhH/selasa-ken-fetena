72962e20d5450fce028953bec4da86ffef3d0811
Amh <abaams78@gmail.com>
2020-10-06 23:16:31 -0500

    Insert into BST soln 1

--- src/main/java/com/example/leet/october/week1/Day6.java
+++ src/main/java/com/example/leet/october/week1/Day6.java
@@ -87,4 +87,27 @@ public class Day6 {
 
         return builder.toString();
     }
+
+    public TreeNode insertIntoBST1(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        TreeNode current = root;
+        while (true) {
+            if (current.val < val) {
+                if (current.right != null) {
+                    current = current.right;
+                } else {
+                    current.right = new TreeNode(val);
+                    break;
+                }
+            } else {
+                if (current.left != null) {
+                    current = current.left;
+                } else {
+                    current.left = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
 }
