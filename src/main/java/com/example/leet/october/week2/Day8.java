bc4e9f27a85b1105cccb2810531047482eb7b739
Amh <abaams78@gmail.com>
2020-10-08 23:14:46 -0500

    Binary search soln

--- src/main/java/com/example/leet/october/week2/Day8.java
+++ src/main/java/com/example/leet/october/week2/Day8.java
@@ -26,5 +26,47 @@ package com.example.leet.october.week2;
  * The value of each element in nums will be in the range [-9999, 9999].
  */
 public class Day8 {
+    public static int search(int[] nums, int target) {
+        return search(nums, target, 0, nums.length - 1);
+    }
 
+    private static int search(int[] nums, int target, int left, int right) {
+        int mid = (right - left)/ 2 + left;
+        if(target == nums[mid])
+            return mid;
+        if(target == nums[left])
+            return left;
+        if(target == nums[right])
+            return right;
+        if(target < nums[mid] && target > nums[left])
+            return search(nums, target, left, mid);
+        if (target  > nums[mid] && target < nums[right])
+            return search(nums, target, mid + 1, right);
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{-1,0,3,5,9,12}, 9));
+        System.out.println(search2(new int[]{-1,0,3,5,9,12}, 2));
+        System.out.println(search2(new int[]{0,5}, 5));
+    }
+
+    public static int search2(int[] nums, int target) {
+
+        int l = 0, r = nums.length - 1;
+        int result = -1;
+
+        while( l <= r ) {
+            int mid = l + (r - l)/2;
+            if(nums[mid] < target) {
+                l = mid + 1;
+            }else if(nums[mid] == target){
+                result = mid;
+                return result;
+            }else {
+                r = mid - 1;
+            }
+        }
+        return result;
+    }
 }
