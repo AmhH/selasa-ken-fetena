3a5e6fefd3a2ade50fb9c6137f256b67315ef709
Amh <abaams78@gmail.com>
2020-10-09 23:01:16 -0500

    Serialize and deserialize BST soln

--- src/main/java/com/example/leet/october/week2/Day9.java
+++ src/main/java/com/example/leet/october/week2/Day9.java
@@ -34,12 +34,54 @@ public class Day9 {
 
         // Encodes a tree to a single string.
         public String serialize(TreeNode root) {
+            if(root == null) return "";
+            StringBuilder sb = new StringBuilder();
+            preorder(root, sb);
+            return sb.toString().trim();
+        }
 
+        private void preorder(TreeNode root, StringBuilder sb) {
+            if(root == null) return;
+            sb.append(root.val + " ");
+            preorder(root.left, sb);
+            preorder(root.right, sb);
         }
 
         // Decodes your encoded data to tree.
         public TreeNode deserialize(String data) {
+            if(data.isEmpty()) return null;
+            String[] preorder = data.split(" ");
+            return deserialize(preorder, 0, preorder.length - 1);
+        }
 
+        public TreeNode deserialize(String[] preorder, int start, int end) {
+            if(start > end) return null;
+            TreeNode root = new TreeNode(Integer.parseInt(preorder[start]));
+            int index;
+            for(index = start; index <= end; index++) {
+                if(Integer.parseInt(preorder[index]) > Integer.parseInt(preorder[start]))
+                    break;
+            }
+
+            root.left = deserialize(preorder, start + 1, index - 1);
+            root.right = deserialize(preorder, index, end);
+
+            return root;
+        }
+    }
+
+    public class Codec0 {
+
+        TreeNode r;
+        // Encodes a tree to a single string.
+        public String serialize(TreeNode root) {
+            this.r = root;
+            return "";
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(String data) {
+            return r;
         }
     }
 
