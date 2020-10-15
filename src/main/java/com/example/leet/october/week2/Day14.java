a66040f9bde60d78f959fff02ed2120ca3e8a97b
Amh <abaams78@gmail.com>
2020-10-14 21:59:27 -0500

    House Robber II soln

--- src/main/java/com/example/leet/october/week2/Day14.java
+++ src/main/java/com/example/leet/october/week2/Day14.java
@@ -40,7 +40,20 @@ package com.example.leet.october.week2;
  */
 public class Day14 {
 
+    public static int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+    }
 
+    private static int rob(int[] num, int low, int high) {
+        int include = 0, exclude = 0;
+        for (int j = low; j <= high; j++) {
+            int i = include, e = exclude;
+            include = e + num[j];
+            exclude = Math.max(e, i);
+        }
+        return Math.max(include, exclude);
+    }
 
     public static void main(String[] args) {
         System.out.println(rob(new int[]{2,3,2}));//3
