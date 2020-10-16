9ebb2e160a683f0273bc24db5502cb5b127e6e8b
Amh <abaams78@gmail.com>
2020-10-15 20:26:32 -0500

    Rotate Array soln

--- src/main/java/com/example/leet/october/week3/Day15.java
+++ src/main/java/com/example/leet/october/week3/Day15.java
@@ -50,4 +50,29 @@ package com.example.leet.october.week3;
  * cyclic-dependencies between elements.
  */
 public class Day15 {
+
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5,6,7};
+        int[] arr2 = {-1,-100,3,99};
+        rotate(arr1, 3);
+        rotate(arr2, 2);
+        System.out.println(arr1);
+        System.out.println(arr2);
+    }
 }
