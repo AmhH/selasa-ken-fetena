c0c2d6e92c0a5f26d213be21bb21732c504c6275
Amh <abaams78@gmail.com>
2020-10-21 16:59:30 -0500

    Astroid Collision soln

--- src/main/java/com/example/leet/october/week3/Day21.java
+++ src/main/java/com/example/leet/october/week3/Day21.java
@@ -1,5 +1,10 @@
 package com.example.leet.october.week3;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Asteroid Collision
  * We are given an array asteroids of integers representing asteroids in a row.
@@ -9,8 +14,6 @@ package com.example.leet.october.week3;
  *
  * Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If
  * both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
- *
- *
  *
  * Example 1:
  *
@@ -34,7 +37,6 @@ package com.example.leet.october.week3;
  * Explanation: The -2 and -1 are moving left, while the 1 and 2 are moving right. Asteroids moving the same
  * direction never meet, so no asteroids will meet each other.
  *
- *
  * Constraints:
  *
  * 1 <= asteroids <= 104
@@ -44,7 +46,103 @@ package com.example.leet.october.week3;
  * Say a row of asteroids is stable. What happens when a new asteroid is added on the right?
  */
 public class Day21 {
-    public int[] asteroidCollision(int[] asteroids) {
+    public static int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> s = new LinkedList<>();
+        for(int i: asteroids){
+            if(i > 0){
+                s.push(i);
+            }else{// i is negative
+                while(!s.isEmpty() && s.peek() > 0 && s.peek() < Math.abs(i)){
+                    s.pop();
+                }
+                if(s.isEmpty() || s.peek() < 0){
+                    s.push(i);
+                }else if(i + s.peek() == 0){
+                    s.pop(); //equal
+                }
+            }
+        }
+        int[] res = new int[s.size()];
+        for(int i = res.length - 1; i >= 0; i--){
+            res[i] = s.pop();
+        }
+        return res;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{5,10,-5})));//[5,10]
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{8,-8})));//[]
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{10,2,-5})));//[10]
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{-2,-1,1,2})));//[-2,-1,1,2]
+    }
+
+    public int[] asteroidCollision1(int[] asteroids) {
+        if (asteroids == null || asteroids.length == 0) {
+            return null;
+        }
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int a : asteroids) {
+            if (a > 0) {
+                stack.addLast(a);
+            } else if (a < 0) {
+                boolean isExploded = false;
+                while (!stack.isEmpty() && stack.peekLast() > 0 && stack.peekLast() <= -a) {
+                    int temp = stack.removeLast();
+                    if (temp == -a) {
+                        isExploded = true;
+                        break;
+                    }
+                }
+                if ((stack.isEmpty() || stack.peekLast() < 0) && !isExploded) {
+                    stack.addLast(a);
+                }
+
+            }
+        }
+        int[] res = new int[stack.size()];
+        for (int i = res.length - 1; i >= 0; --i) {
+            res[i] = stack.removeLast();
+        }
+
+        return res;
+    }
+
+    public int[] asteroidCollision2(int[] asteroids) {
+        if (asteroids == null || asteroids.length == 0) {
+            return null;
+        }
+
+        // two pointers
+        // let end be a pointer such that asteroids[end] contains the asteroids that is safe for now
+        // if asteroids[end] is negative and asteroids[i] is positive, then put i to end + 1, and update end to end + 1
+        // if asteroids[end] and asteroids [i] are both negative or position, put i to end + 1 and update end to end + 1
+        // if asteroids[end] is positive and asteroids[i] is negative,
+        //    while (end >= 0 && asteroids[end] <= -asteroids[i]) end--
+
+        int end = 0;
+        for (int i = 1; i < asteroids.length; ++i) {
+            if (asteroids[i] > 0) {
+                asteroids[++end] = asteroids[i];
+            } else if (asteroids[i] < 0) {
+                boolean isExploded = false;
+                while (end >= 0 && asteroids[end] > 0 && asteroids[end] <= -asteroids[i]) {
+                    int temp = asteroids[end--];
+                    if (temp == -asteroids[i]) {
+                        isExploded = true;
+                        break;
+                    }
+                }
+                if ((end < 0 || asteroids[end] < 0) && !isExploded) {
+                    asteroids[++end] = asteroids[i];
+                }
+            }
+        }
+        int[] res = new int[end + 1];
+        for (int i = 0; i < end + 1; ++i) {
+            res[i] = asteroids[i];
+        }
+        return res;
     }
 }
