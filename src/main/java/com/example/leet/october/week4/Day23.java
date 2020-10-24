c5f9b52c9870319ae6b3386cca037ccd524d6ce6
Amh <abaams78@gmail.com>
2020-10-23 20:38:11 -0500

    132 pattern soln

--- src/main/java/com/example/leet/october/week4/Day23.java
+++ src/main/java/com/example/leet/october/week4/Day23.java
@@ -1,5 +1,7 @@
 package com.example.leet.october.week4;
 
+import java.util.Stack;
+
 /**
  * 132 Pattern
  * Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k]
@@ -35,7 +37,23 @@ package com.example.leet.october.week4;
  */
 public class Day23 {
     public static boolean find132pattern(int[] nums) {
-
+        if (nums.length < 3)
+            return false;
+        Stack<Integer> stack = new Stack<>();
+        int[] min = new int[nums.length];
+        min[0] = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            min[i] = Math.min(min[i - 1], nums[i]);
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if (nums[j] > min[j]) {
+                while (!stack.isEmpty() && stack.peek() <= min[j])
+                    stack.pop();
+                if (!stack.isEmpty() && stack.peek() < nums[j])
+                    return true;
+                stack.push(nums[j]);
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
