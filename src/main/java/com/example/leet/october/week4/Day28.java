b83c393d5a9862642ab707672c0811f607b215b8
Amh <abaams78@gmail.com>
2020-10-28 22:16:50 -0500

    Summary Ranges soln

--- src/main/java/com/example/leet/october/week4/Day28.java
+++ src/main/java/com/example/leet/october/week4/Day28.java
@@ -1,5 +1,7 @@
 package com.example.leet.october.week4;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -56,6 +58,57 @@ import java.util.List;
 public class Day28 {
 
     public static List<String> summaryRanges(int[] nums) {
+        int length = nums.length;
+        List<String> result = new LinkedList<>();
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            while (i < length - 1 && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+            if (num != nums[i]) {
+                result.add(num + "->" + nums[i]);
+            } else {
+                result.add(num + "");
+            }
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(summaryRanges(new int[]{0,1,2,4,5,7}));//["0->2","4->5","7"]
+        System.out.println(summaryRanges(new int[]{0,2,3,4,6,8,9}));//["0","2->4","6","8->9"]
+        System.out.println(summaryRanges(new int[]{}));//[]
+        System.out.println(summaryRanges(new int[]{-1}));//["-1"]
+        System.out.println(summaryRanges0(new int[]{0}));//["0"]
+    }
+
+    public static List<String> summaryRanges0(int[] nums) {
+        List<String> list = new ArrayList<>();
+        if(null == nums || nums.length == 0)
+            return list;
+        boolean arrowInserted = false;
+        int prev = nums[0];
+        StringBuilder str = new StringBuilder();
+        str.append(prev);
+        for(int i = 1; i < nums.length; i++) {
+            if(prev + 1 == nums[i]) {
+                if(arrowInserted == false) {
+                    str.append("->");
+                    arrowInserted = true;
+                }
+            } else {
+                if(arrowInserted)
+                    str.append(prev);
+                list.add(str.toString());
+                str = new StringBuilder();
+                str.append(nums[i]);
+                arrowInserted = false;
+            }
+            prev = nums[i];
+        }
+        if(arrowInserted)
+            str.append(prev);
+        list.add(str.toString());
+        return list;
     }
 }
