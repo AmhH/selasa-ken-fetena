53b533cd6b4110839b2d48e618e80b3e1e8e0d4f
Amh <abaams78@gmail.com>
2020-10-30 20:37:55 -0500

    longest increasing subsequence soln

--- src/main/java/com/example/leet/october/week5/Day30.java
+++ src/main/java/com/example/leet/october/week5/Day30.java
@@ -1,5 +1,7 @@
 package com.example.leet.october.week5;
 
+import java.util.Arrays;
+
 /**
  * Number of Longest Increasing Subsequence
  * Given an integer array nums, return the number of longest increasing subsequences.
@@ -27,7 +29,33 @@ package com.example.leet.october.week5;
 public class Day30 {
 
     public static int findNumberOfLIS(int[] nums) {
+        int N = nums.length;
+        if (N <= 1) return N;
+        int[] lengths = new int[N]; //lengths[i] = length of longest ending in nums[i]
+        int[] counts = new int[N]; //count[i] = number of longest ending in nums[i]
+        Arrays.fill(counts, 1);
 
+        for (int j = 0; j < N; ++j) {
+            for (int i = 0; i < j; ++i) if (nums[i] < nums[j]) {
+                if (lengths[i] >= lengths[j]) {
+                    lengths[j] = lengths[i] + 1;
+                    counts[j] = counts[i];
+                } else if (lengths[i] + 1 == lengths[j]) {
+                    counts[j] += counts[i];
+                }
+            }
+        }
+
+        int longest = 0, ans = 0;
+        for (int length: lengths) {
+            longest = Math.max(longest, length);
+        }
+        for (int i = 0; i < N; ++i) {
+            if (lengths[i] == longest) {
+                ans += counts[i];
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
